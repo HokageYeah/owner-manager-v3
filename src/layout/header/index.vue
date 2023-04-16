b3b6ca323b2d16056d71ec1cd33e0eb296af9298
余晔 <18838280615@163.com>
2023-04-16 13:19:49 +0800

    Css3 新增属性 resize 需要配合overflow使用

--- src/layout/header/index.vue
+++ src/layout/header/index.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>头部</div>
+    <div id="resize">头部</div>
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
 
 </script>
-<style scoped></style>
+<style scoped lang='less'>
+// Css3 新增属性 resize 需要配合overflow使用
+#resize {
+    border: 1px solid red;
+    resize: both;
+    overflow: hidden;
+}
+</style>
