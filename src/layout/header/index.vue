55551552a60e4be763df84753fd147bc05f49a3b
yuye <18838280615@163.com>
2023-04-27 17:44:05 +0800

    代码提交

--- src/layout/header/index.vue
+++ src/layout/header/index.vue
@@ -2,11 +2,16 @@
   <div class="yy-test">
     头部
     <div class="yy-test__input">测试一下</div>
+    <btn />
   </div>
 </template>
 
 <script setup lang="ts">
-// import { ref, reactive } from 'vue';
+import { h } from 'vue';
+
+const btn = () => {
+  return h('button', () => {}, '点击');
+};
 </script>
 <style scoped lang="scss">
 @include b(test) {
